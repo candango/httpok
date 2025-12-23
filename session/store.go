16cf6eddbc62dab12afbfeb76e99a01e33fe2788
Flavio Garcia <piraz@candango.org>
2025-12-23 14:41:17 -0500

    feat(store): add store interface to the project
    Refs: #21

---
+++ session/store.go
@@ -0,0 +1,35 @@
+package session
+
+import (
+	"context"
+)
+
+// Store is a generic key-value engine where the key is a string (such as a
+// session ID, filename, etc.) and the value can be either opaque binary
+// ([]byte) or text (string).
+type Store interface {
+	// Delete removes the specific entry (by id) from the store.
+	Delete(ctx context.Context, id string) error
+
+	// Exists returns true if the key exist.
+	Exists(ctx context.Context, id string) (bool, error)
+
+	// Get retrieves raw data as []byte for the given id (key).
+	Get(ctx context.Context, id string) ([]byte, error)
+
+	// GetString retrieves the string value for the given id.
+	GetString(id string, ctx context.Context) (string, error)
+
+	// Set saves or updates a value for the given id, updating the LastUpdate
+	// time.
+	Set(ctx context.Context, id string, val []byte) error
+
+	// SetString stores a string value as bytes.
+	SetString(ctx context.Context, id string, val string) error
+
+	// Start initializes the store.
+	Start(ctx context.Context) error
+
+	// Stop tears down resources.
+	Stop(ctx context.Context) error
+}
